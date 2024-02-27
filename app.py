aab79052b2e1cf36a56703927aed2554e4f0e7af
Mohit <mohitsinghchandel785@gmaiil.com>
2024-02-28 03:02:03 +0530

    login and signup

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
 import os
+from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
-
+import re
 app = Flask(__name__)
 
 # Initialize Firebase Admin
@@ -44,6 +44,72 @@ def create_profile():
 
     return jsonify({"message": "Profile created successfully", "profile": profile_data}), 201
 
+
+def validate_email(email):
+    # Regular expression for validating an email
+    pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
+    if pattern.match(email):
+        return True
+    else:
+        return False
+
+@app.route('/signup',methods=['POST'])
+def signup():
+    if not request.json:
+        return jsonify({"status:":"error","message":"Request must be JSON"}), 400
+    
+    email=request.json.get('email')
+    password=request.json.get('password')
+    
+    doc_ref=db.collection('user').document(email)
+    if doc_ref.get().exists:
+        return jsonify({"status":"error","message":"User already exists"}), 400
+    
+    if validate_email(email)==False:
+        return jsonify({"status":"error","message":"Invalid email"}), 400
+    
+    if check_password(password)==False:
+        return jsonify({"status":"error","message":"Invalid password"}), 400
+    
+    
+    doc_ref.set({
+        'password':password
+    })
+    return jsonify({"status":"success","message":"User created successfully"}), 201
+        
+def check_password(password):
+    # Regular expression to check if password is strong
+    # ^(?=.*[a-z])      : Ensure string has one lowercase letter.
+    # (?=.*[A-Z])       : Ensure string has one uppercase letter.
+    # (?=.*\d)          : Ensure string has one digit.
+    # (?=.*[!@#$%^&*])  : Ensure string has one special character.
+    # .{8,}             : Ensure string is of length 8.
+    pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$')
+
+    if pattern.match(password):
+        return True
+    else:
+        return False
+    
+    
+@app.route('/login',methods=['GET'])
+def login():
+    
+    email=request.args.get('email')
+    password=request.args.get('password')
+    
+    
+    doc_ref=db.collection('user').document(email)
+    if not doc_ref.get().exists:
+        return jsonify({"status":"error","message":"User does not exist"}), 400
+    
+    doc=doc_ref.get()
+    if doc.to_dict()['password']!=password:
+        return jsonify({"status":"error","message":"Invalid password"}), 400
+    
+    return jsonify({"status":"success","message":"User logged in successfully"}), 200    
+        
+ 
 @app.route('/get-profile', methods=['GET'])
 def get_profile():
     email = request.args.get('email')
